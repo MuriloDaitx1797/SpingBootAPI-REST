93c47a406204452451674e147357d6024d806bde
MuriloDaitx1797 <92128926+MuriloDaitx1797@users.noreply.github.com>
2021-10-12 16:16:27 -0300

    TopicoDTO
    TopicsController: retorna apenas atributos da classe necessário

--- src/main/java/com/API_REST_Alura/forum/controller/TopicsController.java
+++ src/main/java/com/API_REST_Alura/forum/controller/TopicsController.java
@@ -6,18 +6,19 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.API_REST_Alura.forum.controller.dto.TopicoDTO;
 import com.API_REST_Alura_Forum.Model.Curso;
 import com.API_REST_Alura_Forum.Model.Topico;
 
-@Controller
+@RestController
 public class TopicsController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Sprin", "Programacao"));
+	public List<TopicoDTO> lista(){
+		Topico topico = new Topico("Duuvida", "Duvida com Spring", new Curso("Sprin", "Programacao"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
 	}
 }
